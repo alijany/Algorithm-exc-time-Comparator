c1e6a3eb52c1b24bb3028b834a9b2527d56c6a15
alijany <mh.alijany@gamil.com>
2019-10-23 02:49:02 +0330

    append chart

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import "./style/costume.scss";
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/collapse';
+
+import {chart} from './lib/chart'
---
+++ src/lib/chart.js
@@ -0,0 +1,40 @@
+import ApexCharts from 'apexcharts';
+
+var chartOptions = {
+
+    chart: {
+        height: '95%',
+        type: 'line',
+        toolbar: {
+            show: false
+        }
+    },
+    markers: {
+        size: 6,
+        hover: {
+            size: 8,
+            sizeOffset: 3
+        }
+    },
+    stroke: {
+        curve: 'smooth'
+    },
+    series: [],
+    xaxis: {
+        title: {
+            text: 'quantity'
+        }
+    },
+    yaxis: {
+        title: {
+            text: 'Time (millie second)'
+        }
+    },
+    legend: {
+        position: 'top',
+        horizontalAlign: 'right',
+    }
+}
+
+export var chart = new ApexCharts($("#chart")[0], chartOptions);
+chart.render();
