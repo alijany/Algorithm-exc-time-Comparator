a6aeafc1a0a4d52f247592af416c70740fcfe6e3
alijany <mh.alijany@gamil.com>
2019-10-23 03:16:53 +0330

    apend algorithm function

--- src/index.js
+++ src/index.js
@@ -20,3 +20,20 @@ runWorker.onmessage = function (response) {
     sendToWorker.resolve(response.data);
 }
 // ***************************
+
+var mainAlgorithm = 0;
+var $algoList = $("#mainAlgorithm");
+var $loopCount = $("#inputLoop");
+var loopCount = $loopCount.val();
+
+function getCurrentAlgo() {
+    return algorithms[mainAlgorithm].main;
+}
+
+function appendAlgorithmsToList() {
+    var temp = '';
+    algorithms.forEach((algo, index) => {
+        temp += `<option value="${index}">${algo.name}</option>`
+    });
+    $algoList.append($(temp));
+}
