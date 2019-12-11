786ca4234f2f669e5847063b040c79c405a03b79
alijany <mh.alijany@gamil.com>
2019-12-11 23:34:52 +0330

    add new algorithm

--- src/algorithms/algorithmsLoader.js
+++ src/algorithms/algorithmsLoader.js
@@ -8,20 +8,25 @@ import algo5 from '!!raw-loader!./primeNumber3.js';
 import algo6 from '!!raw-loader!./power1.js';
 import algo7 from '!!raw-loader!./power2.js';
 import algo8 from '!!raw-loader!./nQueen.js';
+import algo9 from '!!raw-loader!./hanoi.js';
 
 var algorithms_assignment1 = [
-    { name: "فیبوناتچی مربع کامل", main: algo1, series: undefined, visible: true },
-    { name: "فیبوناتچی حلقه", main: algo2, series: undefined, visible: true },
-    { name: "عدد اول عادی", main: algo3, series: undefined, visible: true },
-    { name: "عدد اول فرد", main: algo4, series: undefined, visible: true },
-    { name: "عدد اول رادیکالی", main: algo5, series: undefined, visible: true },
-    { name: "توان عادی", main: algo6, series: undefined, visible: true },
-    { name: "توان بازگشتی", main: algo7, series: undefined, visible: true }
+    { name: 'فیبوناتچی مربع کامل', main: algo1, series: undefined, visible: true },
+    { name: 'فیبوناتچی حلقه', main: algo2, series: undefined, visible: true },
+    { name: 'عدد اول عادی', main: algo3, series: undefined, visible: true },
+    { name: 'عدد اول فرد', main: algo4, series: undefined, visible: true },
+    { name: 'عدد اول رادیکالی', main: algo5, series: undefined, visible: true },
+    { name: 'توان عادی', main: algo6, series: undefined, visible: true },
+    { name: 'توان بازگشتی', main: algo7, series: undefined, visible: true }
 ];
 
 
 var algorithms_assignment2 = [
-    { name: "nQueen", main: algo8, series: undefined, visible: true }
+    { name: 'nQueen', main: algo8, series: undefined, visible: true }
 ];
 
-export var algorithms = [algorithms_assignment1, algorithms_assignment2];
+var algorithms_assignment3 = [
+    { name: 'hanoi', main: algo9, series: undefined, visible: true }
+];
+
+export var algorithms = [algorithms_assignment1, algorithms_assignment2, algorithms_assignment3];
---
+++ src/algorithms/hanoi.js
@@ -0,0 +1,43 @@
+// This function will recursively store all the moves and return the moves information as Array
+
+algorithm = {
+    initialize: function () {
+        this.count = 2;
+    },
+
+    update: function () {
+        this.count += 3;
+    },
+
+    hanoi: function (count, source, auxiliary, destination, moves) {
+        moves = moves || [];
+
+        if (count === 1) {
+            moves.push({
+                'disk': 1,
+                'from': source,
+                'to': destination
+            });
+        }
+
+        else {
+            // Move 'n-1' disks from 'source' to 'aux'
+            this.hanoi(count - 1, source, destination, auxiliary, moves);
+
+            moves.push({
+                'disk': count,
+                'from': source,
+                'to': destination
+            });
+
+            // Move (n-1) disks from 'aux' to 'destination'
+            this.hanoi(count - 1, auxiliary, source, destination, moves);
+        }
+
+        return moves;
+    },
+
+    run: function () {
+        return this.hanoi(this.count, 'A', 'B', 'C');
+    }
+};
